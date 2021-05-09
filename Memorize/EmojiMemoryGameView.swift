a9ad20bce014ca7e0cbb79a823174d58466f32a4
Xin Tu <aaron@kljsandjb.me>
2021-05-09 21:16:22 +0200

    animation

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -14,14 +14,20 @@ struct EmojiMemoryGameView: View {
         HStack() {
             Text(emojiGame.themeName())
                 .italic()
-            Button("New Game") { emojiGame.newEmojiGame() }
-                .buttonStyle(BorderlessButtonStyle())
-                .padding(5)
+            Button("New Game") {
+                withAnimation(.easeInOut) {
+                    emojiGame.newEmojiGame()
+                }
+            }
+            .buttonStyle(BorderlessButtonStyle())
+            .padding(5)
         }
 
         Grid(items: emojiGame.cards) { card in
             CardView(card: card).onTapGesture {
-                emojiGame.choose(card)
+                withAnimation(.linear(duration: 0.75)) {
+                    emojiGame.choose(card)
+                }
             }
             .padding(5)
         }
@@ -38,18 +44,38 @@ struct EmojiMemoryGameView: View {
 
 struct CardView: View {
     var card: MemoryGame<String>.Card
+    
+    @State private var animatedBonusRemaining: Double = 0
 
     var body: some View {
         GeometryReader { geometry in
             if card.isFaceUp || !card.isMatched {
                 ZStack {
-                    Pie(startAngle: Angle.radians(-Double.pi / 2), endAngle: Angle.degrees(20), clockWise: true)
-                        .padding(5)
-                        .opacity(0.4)
+                    Group {
+                        if card.isConsumingBonusTime {
+                            Pie(startAngle: Angle.radians(-Double.pi / 2), endAngle: Angle.degrees(-animatedBonusRemaining * 360 - 90), clockWise: true)
+                                .onAppear {
+                                    animatedBonusRemaining = card.bonusRemaining
+                                    withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+                                        animatedBonusRemaining = 0
+                                    }
+                                }
+                        } else {
+                            Pie(startAngle: Angle.radians(-Double.pi / 2), endAngle: Angle.degrees(-card.bonusRemaining * 360 - 90), clockWise: true)
+                        }
+                    }
+                    .padding(5)
+                    .opacity(0.4)
+                    
                     Text(card.content)
                         .font(Font.system(size: bodySize(for: geometry.size)))
+                        .rotationEffect(Angle.radians(card.isMatched ? Double.pi * 2 : 0))
+                        // animation only applied to changes, which means the view should have been on the screen but got changes, if the view
+                        // e.g. got re-drawn, it won't trigger the animation
+                        .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
                 }
                 .cardify(isFaceUp: card.isFaceUp)
+                .transition(AnyTransition.scale)
             }
         }
     }
